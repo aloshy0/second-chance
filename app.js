6ee85e26f2561d45b96afd39556659fd1951c4ba
aloshy0 <aloshyantony639@gmail.com>
2026-03-04 11:38:22 +0530

    Fixed merge conflict in db.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const authRoutes = require("./routes/authRoutes");
 const app = express();
 
 app.use(express.json());
+app.use(express.static("public"));
 
 /* routes */
 
--- db.js
+++ db.js
@@ -1,19 +1,15 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require("mongodb");
 
-const uri = "mongodb://localhost:27017"; // local MongoDB connection
+const uri = "mongodb://localhost:27017";
 
 const client = new MongoClient(uri);
 
 async function connectToDatabase() {
-    await client.connect();   // <-- this is the line the grader wants
-    console.log("Connected to MongoDB");
+  await client.connect();
+  console.log("Connected to MongoDB");
 
-    const db = client.db("secondChanceDB");
-    return db;
+  const db = client.db("secondChanceDB");
+  return db;
 }
 
-<<<<<<< HEAD
 module.exports = { connectToDatabase };
-=======
-module.exports = { connectToDatabase };
->>>>>>> 4153672bf1547fa7b7c916d06aac61a5cfef1976
